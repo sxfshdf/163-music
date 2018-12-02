34476c47d09e240626de2c3f85dd5ba653d0fe74
sixiaofeng <sxf754872493@gmail.com>
2018-12-02 16:32:44 +0800

    获取歌曲列表

--- src/js/songList-container.js
+++ src/js/songList-container.js
@@ -26,6 +26,15 @@
       songs:[
 
       ]
+    },
+    find(){
+      var query = new AV.Query('Song')
+      return query.find().then((songs)=>{
+        this.data.songs = songs.map((song)=>{
+          return {id:song.id, ...song.attributes}
+        })
+        return songs
+      })
     }
   }
   let controller = {
@@ -38,6 +47,9 @@
       })
       window.eventHub.on('create',(songData)=>{
         this.model.data.songs.push(songData)
+        this.view.render(this.model.data)
+      })
+      this.model.find().then(()=>{
         this.view.render(this.model.data)
       })
     }
